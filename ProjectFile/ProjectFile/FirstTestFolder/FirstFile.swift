1d66fe36c28f16a054d8b9f63c152ddc53805361
Uladzislau_Komar <vladkomar25@gmail.com>
2023-05-19 15:26:02 +0300

    Initial commit

---
+++ ProjectFile/ProjectFile/FirstTestFolder/FirstFile.swift
@@ -0,0 +1,8 @@
+//
+//  FirstFile.swift
+//  ProjectFile
+//
+//  Created by Uladzislau Komar on 19.05.23.
+//
+
+import Foundation
---
+++ ProjectFile/ProjectFile/FirstTestFolder/NestedFirstTestFolder/NestedFirstFile.swift
@@ -0,0 +1,8 @@
+//
+//  NestedFirstFile.swift
+//  ProjectFile
+//
+//  Created by Uladzislau Komar on 19.05.23.
+//
+
+import Foundation
